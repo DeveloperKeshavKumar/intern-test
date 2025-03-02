39f4172458b2c9636be25943a79c91e2a576cedc
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-03-02 23:47:06 +0530

    use browserRouter for routing system

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
-function App() {
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import Products from './components/Products';
+import AddProducts from './components/AddProducts';
+import NotFound from './components/NotFound';
 
+export default function App() {
   return (
     <>
-      Hello World
+      <div>
+        <Navbar />
+        <div>
+          <Routes>
+            {/* Define Routes for each page */}
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/addProducts" element={<AddProducts />} />
+            {/* Default route */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
     </>
-  )
+  );
 }
-
-export default App
